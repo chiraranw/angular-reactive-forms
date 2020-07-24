da7b8ee6b4c71b8fed49e006913c987a75618ea5
Nation Chirara <chiraranw@gmail.com>
2020-07-24 20:55:45 +0200

    Adjusting validation rules at runtime: refactoring the func

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -7,6 +7,7 @@ import {
   Validators,
   FormControl,
   AbstractControl,
+  ValidatorFn,
 } from '@angular/forms';
 
 @Component({
@@ -26,6 +27,7 @@ export class CustomerComponent implements OnInit {
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required]],
+      rating: [null, [ratingValidator(1, 5)]],
       notification: ['email'],
       sendCatalog: [],
     });
@@ -43,4 +45,16 @@ export class CustomerComponent implements OnInit {
   }
 
   save(customerForm): void {}
+}
+
+function ratingValidator(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (
+      c.value !== null &&
+      (isNaN(c.value) || c.value < min || c.value > max)
+    ) {
+      return { rating: true };
+    }
+    return null;
+  };
 }
